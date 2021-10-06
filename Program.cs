d5f9740434155b93c1665697d39ba7bd2bf3064d
gabriel.gutierrez <gabriel.gutierrez@agilethought.com>
2021-10-06 10:53:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Selenium_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("ok" + Keys.Enter);
+            driver.Quit();
+
+        }
+
+
+    }
+}
